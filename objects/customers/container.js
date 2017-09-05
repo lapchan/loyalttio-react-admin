00fddb42425d13716b7d9daeff3187e4739a628a
lurlurbear <lurlurbear@gmail.com>
2017-09-05 14:54:58 +0800

    add UI for  create customer

--- objects/customers/container.js
+++ objects/customers/container.js
@@ -8,6 +8,11 @@ import {Panel} from '../../components/ui/';
 import {Row, Col, Page} from '../../components/ui/Layout';
 import {getMerchantsCustomers} from './api'
 
+import ModalFactory from '../../components/modals/factory';
+import AddCustomer from './addContainer';
+
+let Factory = ModalFactory.modalFromFactory;
+
 const dataSchema = {
   name : 'Customers',
   description : 'cusomters schema',
@@ -39,10 +44,32 @@ class Customers extends Component {
     dispatch(getMerchantsCustomers());
   }
 
+    handleNewNote(customer) {
+      let {dispatch} = this.props;
+      console.log( " create Customer");
+
+      ModalFactory.hide('addCustomerModal');
+    }
+
+    newCustomerClick(e) {
+        e.preventDefault();
+        ModalFactory.show('addCustomerModal');
+    }
+
+
   render() {
     let {customers} = this.props;
     return (
       <Page>
+         <Factory modalref="addCustomerModal" title="Add Customer" factory={AddCustomer} onCreate={(customer) => this.handleNewNote(customer)} />
+         <Row>
+          <div className="pull-right w50 text-right m-r-lg">
+            <button className="btn btn-dark" onClick={(e)=>this.newCustomerClick(e)}> 
+              <i className="fa fa-plus-square"></i>
+              <span className="btn-text">New Customer</span> 
+            </button>
+          </div>
+        </Row>
         <Row>
           <Col size={12}>
             <Panel>
